6a932fb1b5753c73a8d0d73be4182067e9b3ccf8
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2020-09-29 08:44:08 +0100

    Attempt to set resourceFile before run Task will fail if there are errors
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- src/execute/execute.ts
+++ src/execute/execute.ts
@@ -1,15 +1,23 @@
 // Import the necessary tasks
+import * as tl from "azure-pipelines-task-lib"; // task library for Azure DevOps
 import * as task from "../common/taskConfiguration";
 import * as ex from "../common/executeComponent";
 import { join as pathJoin } from "path";
+import { sprintf } from "sprintf-js";
+
+// set the path to the resourceFile
+try {
+  let resourceFile = pathJoin(__dirname, "task.json");
+  tl.debug(sprintf("Resource file path: %s", resourceFile));
+  tl.setResourcePath(resourceFile);
+} catch (err) {
+  tl.setResult(tl.TaskResult.Failed, err);
+}
 
 // Execute the chosen Chef component command with the specified arguments
 async function run() {
 
-  // initialise the required classes
-  let resourceFile = pathJoin(__dirname, "task.json");
-  console.log(resourceFile);
-  let taskConfiguration = new task.TaskConfiguration(resourceFile);
+  let taskConfiguration = new task.TaskConfiguration();
 
   // get the parameters for the task, e.g. the settings that have been provided
   await taskConfiguration.getTaskParameters({}, []);
